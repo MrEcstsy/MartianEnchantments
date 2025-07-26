c6f215722d7e329d50522133fc63a70bea4cfa48
MrEcstsy <57694923+MrEcstsy@users.noreply.github.com>
2025-07-26 04:35:47 -0700

    Added more valid items

--- src/ecstsy/MartianEnchantments/conditions/IsHoldingCondition.php
+++ src/ecstsy/MartianEnchantments/conditions/IsHoldingCondition.php
@@ -26,7 +26,9 @@ class IsHoldingCondition implements ConditionInterface {
         $validItems = [
             'sword' => [ItemTypeIds::WOODEN_SWORD, ItemTypeIds::STONE_SWORD, ItemTypeIds::IRON_SWORD, ItemTypeIds::GOLDEN_SWORD, ItemTypeIds::DIAMOND_SWORD, ItemTypeIds::NETHERITE_SWORD],
             'axe' => [ItemTypeIds::WOODEN_AXE, ItemTypeIds::STONE_AXE, ItemTypeIds::IRON_AXE, ItemTypeIds::GOLDEN_AXE, ItemTypeIds::DIAMOND_AXE, ItemTypeIds::NETHERITE_AXE],
-            'bow' => [ItemTypeIds::BOW]
+            'bow' => [ItemTypeIds::BOW],
+            'pickaxe' => [ItemTypeIds::WOODEN_PICKAXE, ItemTypeIds::STONE_PICKAXE, ItemTypeIds::IRON_PICKAXE, ItemTypeIds::GOLDEN_PICKAXE, ItemTypeIds::DIAMOND_PICKAXE, ItemTypeIds::NETHERITE_PICKAXE],
+            'shovel' => [ItemTypeIds::WOODEN_SHOVEL, ItemTypeIds::STONE_SHOVEL, ItemTypeIds::IRON_SHOVEL, ItemTypeIds::GOLDEN_SHOVEL, ItemTypeIds::DIAMOND_SHOVEL, ItemTypeIds::NETHERITE_SHOVEL],
         ];
         
         if (isset($validItems[$requiredItem])) {
