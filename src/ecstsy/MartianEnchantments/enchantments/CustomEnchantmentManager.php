8193a6006188faf3fd71fd50908c7c3ee80fb6d7
MrEcstsy <57694923+MrEcstsy@users.noreply.github.com>
2026-01-11 15:48:01 -0800

    Enhance CustomEnchantmentManager with new methods
    Added methods to retrieve enchantments, check for enchantments, and get enchantment levels.

--- src/ecstsy/MartianEnchantments/enchantments/CustomEnchantmentManager.php
+++ src/ecstsy/MartianEnchantments/enchantments/CustomEnchantmentManager.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace ecstsy\MartianEnchantments\enchantments;
 
-use CustomEnchantmentInstance;
 use ecstsy\MartianEnchantments\utils\Utils;
 use pocketmine\item\Item;
 use pocketmine\nbt\tag\CompoundTag;
@@ -50,6 +49,45 @@ final class CustomEnchantmentManager {
         }
 
         Utils::updateGlowEffect($item);
+    }
+
+    public static function getEnchantments(Item $item): array
+    {
+        $root = $item->getNamedTag();
+        $enchTag = $root->getCompoundTag("MartianCES");
+        if ($enchTag === null) {
+            return [];
+        }
+
+        $result = [];
+        foreach ($enchTag->getValue() as $name => $tag) {
+            $level = $tag->getValue();
+            $custom = CustomEnchantments::getEnchantmentByName((string)$name);
+            if ($custom !== null) {
+                $result[$custom->getName()] = $level;
+            }
+        }
+        return $result;
+    }
+
+    public static function hasEnchantment(Item $item, string $enchantName): bool {
+        $root = $item->getNamedTag();
+        $tag = $root->getCompoundTag("MartianCES");
+        return $tag !== null && $tag->getTag($enchantName) !== null;
+    }
+    
+    public static function getLevel(Item $item, string $enchantName): int {
+        $root = $item->getNamedTag();
+        $tag = $root->getCompoundTag("MartianCES");
+        if ($tag === null || !$tag->getTag($enchantName)) {
+            return 0;
+        }
+        return $tag->getInt($enchantName);
+    }
+
+    // hmmm.... ._.
+    public static function getEnchantment(string $name): ?CustomEnchantment {
+        return CustomEnchantments::getEnchantmentByName($name);
     }
 
     public function sortEnchantmentsByRarity(array $enchantments): array {
